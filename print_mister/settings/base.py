31f8d02320cf69c52987469f5d016b28cff35018
Ruslan Sayfullin <uraltus@yandex.ru>
2018-07-25 14:30:42 +0200

    PM-1 separate settings

--- print_mister/settings/base.py
+++ print_mister/settings/base.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'print_mister.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
